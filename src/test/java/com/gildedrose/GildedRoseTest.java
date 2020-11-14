6fac49b1c44fd69a36c1900bd54499458d72a04f
dieter.jordens <dieter.jordens@continuum.be>
2020-11-14 10:53:25 +0100

    Add a test for the loop, extract method for update quality item and replace for loop with enhanced for loop

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,16 +2,25 @@ package com.gildedrose;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void updatesMultipleItems() {
+        Item[] items = new Item[] {
+                new Item("item1", 1, 2),
+                new Item("item2", 3, 4)
+        };
         GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("foo", app.items[0].name);
-    }
 
+        app.updateQuality();
+
+        Item item1 = app.items[0];
+        assertThat(item1.quality).isEqualTo(1);
+        assertThat(item1.sellIn).isEqualTo(0);
+        Item item2 = app.items[1];
+        assertThat(item2.quality).isEqualTo(3);
+        assertThat(item2.sellIn).isEqualTo(2);
+    }
 }
