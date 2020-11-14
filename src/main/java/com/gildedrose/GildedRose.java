df2aa7176b7690eb01fe2d384626ce3fdfda9693
dieter.jordens <dieter.jordens@continuum.be>
2020-11-14 18:39:06 +0100

    Remove duplicate tests and clean up gilded rose a bit more

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -18,28 +18,18 @@ class GildedRose {
     }
 
     public void updateItem(Item item) {
-        if("Aged Brie".equals(item.name)){
+        if ("Aged Brie".equals(item.name)) {
             ItemHandler itemHandler = new AgedBrieItemHandler();
             itemHandler.update(item);
-        } else if("Sulfuras, Hand of Ragnaros".equals(item.name)){
+        } else if ("Sulfuras, Hand of Ragnaros".equals(item.name)) {
             ItemHandler itemHandler = new LegendaryItemHandler();
             itemHandler.update(item);
-        } else if("Backstage passes to a TAFKAL80ETC concert".equals(item.name)){
+        } else if ("Backstage passes to a TAFKAL80ETC concert".equals(item.name)) {
             ItemHandler itemHandler = new BackStageItemHandler();
             itemHandler.update(item);
-        }
-        else {
-            if (item.quality > 0) {
-                item.quality = item.quality - 1;
-            }
-
-            item.sellIn = item.sellIn - 1;
-
-            if (item.sellIn < 0) {
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1;
-                }
-            }
+        } else {
+            ItemHandler itemHandler = new ItemHandler() {};
+            itemHandler.update(item);
         }
     }
 }
