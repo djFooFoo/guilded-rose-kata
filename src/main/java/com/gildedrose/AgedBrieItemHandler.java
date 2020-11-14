0e33fd35e568bc90a80ef9b4165df2220e7ca2ee
dieter.jordens <dieter.jordens@continuum.be>
2020-11-14 15:47:33 +0100

    Aged brie sell in decrements

--- src/main/java/com/gildedrose/AgedBrieItemHandler.java
+++ src/main/java/com/gildedrose/AgedBrieItemHandler.java
@@ -3,5 +3,6 @@ package com.gildedrose;
 public class AgedBrieItemHandler implements ItemHandler {
     @Override
     public void update(Item item) {
+        item.decrementSellIn();
     }
 }
--- src/test/java/com/gildedrose/AgedBrieItemHandlerTest.java
+++ src/test/java/com/gildedrose/AgedBrieItemHandlerTest.java
@@ -1,5 +1,18 @@
 package com.gildedrose;
 
-class AgedBrieItemHandlerTest {
+import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+class AgedBrieItemHandlerTest {
+    @Test
+    void givenAgedBrieSellInValueDecrements() {
+        Item item = new Item("Aged Brie", 19, 22);
+
+        ItemHandler itemHandler = new AgedBrieItemHandler();
+
+        itemHandler.update(item);
+
+        assertThat(item.sellIn).isEqualTo(18);
+    }
 }
