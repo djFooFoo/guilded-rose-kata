d1ec14153561fa843cc5ed5cb2ece53b05868de2
dieter.jordens <dieter.jordens@continuum.be>
2020-11-14 17:53:41 +0100

    Back stage pass sell in value decrements

---
+++ src/main/java/com/gildedrose/BackStageItemHandler.java
@@ -0,0 +1,8 @@
+package com.gildedrose;
+
+public class BackStageItemHandler implements ItemHandler{
+    @Override
+    public void update(Item item) {
+        item.decrementSellIn();
+    }
+}
---
+++ src/test/java/com/gildedrose/BackStageItemHandlerTest.java
@@ -0,0 +1,18 @@
+package com.gildedrose;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BackStageItemHandlerTest {
+    @Test
+    void givenBackStagePassSellInValueDecrements() {
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 19, 22);
+
+        ItemHandler itemHandler = new BackStageItemHandler();
+
+        itemHandler.update(item);
+
+        assertThat(item.getSellIn()).isEqualTo(18);
+    }
+}
