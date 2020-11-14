49155fa8ca16c017f51060c6166c8a5258322651
dieter.jordens <dieter.jordens@continuum.be>
2020-11-14 18:18:08 +0100

    Simplify gilded rose and remove duplicate tests

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -24,38 +24,20 @@ class GildedRose {
         } else if("Sulfuras, Hand of Ragnaros".equals(item.name)){
             ItemHandler itemHandler = new LegendaryItemHandler();
             itemHandler.update(item);
+        } else if("Backstage passes to a TAFKAL80ETC concert".equals(item.name)){
+            ItemHandler itemHandler = new BackStageItemHandler();
+            itemHandler.update(item);
         }
         else {
-            if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1;
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                    if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
-
-                    if (item.sellIn < 6) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
-                }
+            if (item.quality > 0) {
+                item.quality = item.quality - 1;
             }
 
             item.sellIn = item.sellIn - 1;
 
             if (item.sellIn < 0) {
-                if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.quality > 0) {
-                        item.quality = item.quality - 1;
-                    }
-                } else {
-                    item.quality = 0;
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1;
                 }
             }
         }
